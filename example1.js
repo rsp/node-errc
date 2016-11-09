f29ec8b65a7928726d038c27bbcb1a54a3abdba2
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-11-09 18:35:50 +0100

    add example1

---
+++ example1.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var errc = require('./index.js');
+
+function callWithError(cb) {
+  cb('ERROR');
+}
+
+function callWithData(cb) {
+  cb(null, 'DATA');
+}
+
+var isError = function(obj) {
+  try { return obj instanceof Error; } catch(e) {}
+  return false;
+};
+
+var callback = errc(function(data) {
+  if (isError(data)) {
+    console.log('Error:', data.message);
+  } else {
+    console.log('Success:', data);
+  }
+});
+
+callWithError(callback);
+callWithData(callback);
+
